66cb22707dd6712103ab0d9ca2afa9be1a6f53bb
Illia Redka <illaredka8@gmail.com>
2026-05-29 20:31:28 +0300

    Change single quotes to double quotes

--- railway_service/asgi.py
+++ railway_service/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'railway_service.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "railway_service.settings")
 
 application = get_asgi_application()
