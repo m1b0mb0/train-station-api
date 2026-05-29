d8e13e5c13b08a54de58cb7d064b275e9705de94
Illia Redka <illaredka8@gmail.com>
2026-05-29 21:43:47 +0300

    Add route model

--- railway/models.py
+++ railway/models.py
@@ -11,3 +11,32 @@ class Station(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Route(models.Model):
+    source = models.ForeignKey(
+        Station,
+        on_delete=models.CASCADE,
+        related_name="routes_from"
+    )
+    destination = models.ForeignKey(
+        Station,
+        on_delete=models.CASCADE,
+        related_name="routes_to"
+    )
+    distance = models.PositiveIntegerField()
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["source", "destination"],
+                name="unique_route"
+            ),
+            models.CheckConstraint(
+                condition=~models.Q(source=models.F("destination")),
+                name="source_not_destination"
+            )
+        ]
+
+    def __str__(self):
+        return f"{self.source.name} - {self.destination.name} ({self.distance} km)"
