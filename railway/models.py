9158ef0aa938c78c2fc7f7d7286deb1ad5ba9b0c
Illia Redka <illaredka8@gmail.com>
2026-05-29 21:42:55 +0300

    Add station model

--- railway/models.py
+++ railway/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Station(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+
+    class Meta:
+        ordering = ("name",)
+
+    def __str__(self):
+        return self.name
