b498cb1fdaf87c4273857bb2fbdf240c9f49e1bf
Illia Redka <illaredka8@gmail.com>
2026-05-30 11:41:17 +0300

    Add models to admin panel

--- railway/admin.py
+++ railway/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
 
-# Register your models here.
+from railway.models import (
+    Station,
+    Route,
+    TrainType,
+    Train,
+    Journey,
+    Crew,
+    Order,
+    Ticket
+)
+
+
+class TicketInline(admin.TabularInline):
+    model = Ticket
+    extra = 1
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    inlines = (TicketInline,)
+
+
+admin.site.register(Station)
+admin.site.register(Route)
+admin.site.register(TrainType)
+admin.site.register(Train)
+admin.site.register(Journey)
+admin.site.register(Crew)
+admin.site.register(Ticket)
