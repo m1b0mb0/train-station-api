d19bff7724c1262e0282f2e156784ea1d2a864c2
Illia Redka <illaredka8@gmail.com>
2026-05-30 12:36:52 +0300

    Add django debug toolbar

--- railway_service/urls.py
+++ railway_service/urls.py
@@ -17,8 +17,11 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from django.urls import include, path
+from debug_toolbar.toolbar import debug_toolbar_urls
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/railway/", include("railway.urls", namespace="railway")),
-]
+] + debug_toolbar_urls()
